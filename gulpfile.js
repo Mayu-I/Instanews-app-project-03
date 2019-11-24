2b607d4bd36689ae8064588a5342db6af29d8d03
Mayu Imanaka <imamayu.1215@gmail.com>
2019-11-23 16:21:59 -0800

    add:gulp sass and error

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,28 @@ const gulp = require('gulp'),
     terser = require('gulp-terser'),
     rename = require('gulp-rename'),
     eslint = require('gulp-eslint'),
+    sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    autoprefixer = require('gulp-autoprefixer'),
     browserSync = require('browser-sync').create();
 
+gulp.task("sass", function () {
+    return gulp
+        .src("./stylesheets/styles.scss")
+        .pipe(sass())
+        .pipe(
+            autoprefixer({
+                browsers: ["last 2 versions"]
+            })
+        )
+        .pipe(gulp.dest("./build/css"))
+        .pipe(cssnano())
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest("./build/css"));
+});
+
 gulp.task('scripts', function () {
-    return gulp.src('./js/*.js')
+    gulp.src('./js/*.js')
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError())
@@ -15,7 +33,7 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('browser-sync', function () {
-    browserSync.init({
+    return browserSync.init({
         server: {
             baseDir: "./"
         }
@@ -26,14 +44,15 @@ gulp.task('reload', function (done) {
     browserSync.reload();
     done();
 });
-
 gulp.task('watch', function () {
     gulp.watch("js/*.js", gulp.series('scripts', 'reload'));
+    gulp.watch("./stylesheets/*.scss", gulp.series('sass', 'reload'));
     gulp.watch("index.html", gulp.series('reload'));
 });
 
 
-gulp.task('default', gulp.parallel('browser-sync', 'watch'));
+
+gulp.task('default', gulp.parallel('browser-sync', 'watch', 'scripts', 'sass'));
 
 
 

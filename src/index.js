de9f48e8e936cd0d0141f43dd546b2bfcafe094f
Mayu Imanaka <imamayu.1215@gmail.com>
2019-11-30 20:44:26 -0800

    update:fetch data from api

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
-$(function () {
-    const category = $('.header__sectionWrapper').val();
-    $.ajax({
-        type: 'GET',
-        url: `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=91MnfWevTdr6zkWbGOsM5wjGin9Hej9K`,
-    })
-        .then(
-            data => {
-                console.log(data);
-                // for (let i = 0; i < 6; i++) {
 
-                // }
-            },
-            error => alert('Failed to load')
-        )
+$(function () {
+    const getArticles = () => {
+        const category = $('.header__sectionWrapper').val();
+        $.ajax({
+            type: 'GET',
+            url: `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=91MnfWevTdr6zkWbGOsM5wjGin9Hej9K`,
+        })
+            .then(
+                data => {
+                    // if ($(".articles").is(':empty')) {
+                    //     $(`<p class="articles__empty">Oops! No articles in ${category}...</p>`)
+                    //         .appendTo("content");
+                    // } else {
+                    for (let i = 0; i < 12; i++) {
+                        const link = data.results[i].url;
+                        const img = data.results[i].multimedia[4].url;
+                        const desc = data.results[i].abstract;
+                        $(`<a href="${link}" class="articles__item" style="background-image:url('${img}');" target="_blank">
+                <article>
+                    <p class="articles__desc">${desc}</p>
+                </article>
+            </a>`)
+                            .appendTo(".articles");
+                    }
+                    // }
+                },
+                error => alert('Failed to load')
+            )
+    }
+
+    getArticles();
+    $('.header__sectionWrapper').change(function () {
+        $(".articles").empty();
+
+        getArticles();
+
+    });
 });

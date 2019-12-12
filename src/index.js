6b5a9ee0c0bffb19bb5893dba0656cf7b128d8fd
Mayu Imanaka <imamayu.1215@gmail.com>
2019-12-12 15:18:17 -0800

    add:header animation

--- src/index.js
+++ src/index.js
@@ -4,13 +4,16 @@ $(function () {
         $.ajax({
             type: 'GET',
             url: `https://api.nytimes.com/svc/topstories/v2/${category}.json?api-key=91MnfWevTdr6zkWbGOsM5wjGin9Hej9K`,
+            beforeSend: function () {
+                $('.loader').removeClass('hide');
+            },
+            success: function () {
+                $('.loader').addClass('hide');
+                $('.header').addClass('clicked');
+            }
         })
             .then(
                 data => {
-                    // if ($(".articles").is(':empty')) {
-                    //     $(`<p class="articles__empty">Oops! No articles in ${category}...</p>`)
-                    //         .appendTo("content");
-                    // } else {
                     for (let i = 0; i < 12; i++) {
                         const link = data.results[i].url;
                         const img = data.results[i].multimedia[4].url;
@@ -22,21 +25,29 @@ $(function () {
             </a>`)
                             .appendTo(".articles");
                     }
-                    // }
                 },
                 error => alert('Failed to load')
             )
     }
 
     getArticles();
+
     $('.header__sectionWrapper').change(function () {
         $(".articles").empty();
-
         getArticles();
-
     });
 
-    $()
+    //abstract
+    $("a").hover(
+        function () {
+            console.log('hover!')
+            $('.articles__desc')
+                .toggleClass('hide')
+                .toggleClass('show')
+        }
+    );
+
+
 });
 
 
